dde3ea3bd680e48d64645f157286dc75028b0b53
Meelis Karulin <karulin@mediatis.de>
2021-12-10 17:08:18 +0100

    Replace SwiftMailer with Symfony Mailer

--- Classes/Manager/MailManager.php
+++ Classes/Manager/MailManager.php
@@ -3,25 +3,24 @@
 namespace Mediatis\FormrelayMail\Manager;
 
 use FormRelay\Mail\Manager\MailManagerInterface;
-use Swift_Mailer;
-use Swift_Message;
+use Symfony\Component\Mime\Email;
 use TYPO3\CMS\Core\Mail\Mailer;
 use TYPO3\CMS\Core\Mail\MailMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class MailManager implements MailManagerInterface
 {
-    protected function getMailer(): Swift_Mailer
+    protected function getMailer(): Mailer
     {
         return GeneralUtility::makeInstance(Mailer::class);
     }
 
-    public function createMessage(): Swift_Message
+    public function createMessage(): Email
     {
         return GeneralUtility::makeInstance(MailMessage::class);
     }
 
-    public function sendMessage(Swift_Message $message): bool
+    public function sendMessage(Email $message): bool
     {
         if ($message instanceof MailMessage) {
             return (bool)$message->send();
